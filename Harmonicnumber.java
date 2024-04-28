ce0827fcf22b807d4916fb199f4587fb4a3f846e
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-28 21:18:28 +0300

    added Harmonicnumber.java

---
+++ Harmonicnumber.java
@@ -0,0 +1,16 @@
+//PROGRAM THAT CALCULATE HARMONIK NUMBER UP TO N.(USER WİLL ENTER THE N).
+
+import java.util.Scanner;
+
+public class Harmonicnumber {
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = get.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        System.out.printf("Harmonic number up to %d: %.2f", n, sum);
+    }
+}
