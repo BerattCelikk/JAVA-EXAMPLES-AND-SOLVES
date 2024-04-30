16b6e09817e5fee18cdd5804c02755758c100255
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-30 23:52:30 +0300

    added Combination.java

---
+++ Combination.java
@@ -0,0 +1,24 @@
+//Calculate and display COMBİNATİON(C(n,k)) by using for loop.
+
+/*import java.util.Scanner;
+public class Combination{
+    public static void main (String[] args) {
+        Scanner get=new Scanner(System.in);
+        System.out.println("enter C(n,k) respectively:");
+        int c=get.nextInt();
+        int k=get.nextInt();
+        int result=1;
+        if(k==0){
+            System.out.printf("C(%d,%d) is not a combination",c,k);
+        }else{
+            for(int i=1;i<=c;i++){
+            result*=i;
+            }
+            for(int i=1;i<=k;i++){
+            result/=i;
+            }
+            System.out.printf("C(%d,%d) = %d",c,k,result);
+        }
+        
+    }
+}*/
