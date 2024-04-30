131e972041cf18eafc7cd6e4129300745d9180fc
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-30 23:53:21 +0300

    added Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+//DISPLAY FACTORIAL VALUE OF N ENTERED BY THE USER.
+
+/*import java.util.Scanner;
+public class Factorial{
+    public static void main (String[] args) {
+        Scanner get = new Scanner(System.in);
+        System.out.println("enter a number:");
+        int number=get.nextInt();
+        int factorial=1;
+        for(int i=1;i<=number;i++){
+            factorial*=i;
+        }
+        System.out.printf("factorial of %d is %d",number,factorial);
+    }
+}*/
