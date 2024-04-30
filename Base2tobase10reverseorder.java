ffb25470a0eb5b4695e3cd76c78dd8111ab6693c
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-30 23:52:12 +0300

    added Base2tobase10reverseorder.java

---
+++ Base2tobase10reverseorder.java
@@ -0,0 +1,13 @@
+//DISPLAY THE BASE-2 SYSTEM EQUIVALENT OF A NUMBER ENTERED BY THE USER AS BASE-10 SYSTEM IN REVERSE ORVER.
+
+/*import java.util.Scanner;
+public class Base2tobase10reverseorder{
+    public static void main (String[] args) {
+        Scanner get =new Scanner(System.in);
+        System.out.println("enter a number in base-10 system:");
+        int number=get.nextInt();
+        for(int i=number;i>0;i/=2){
+            System.out.printf("%d",i%2);
+        }
+    }
+}*/
