1d894261c7aa36670915ef65584b5918dd330173
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-30 23:52:54 +0300

    added Displaynumbersperline.java

---
+++ Displaynumbersperline.java
@@ -0,0 +1,47 @@
+//THIS FILE INCLUDES TOTAL 3 QUESTIONS.
+
+// A-) DISPLAY ALL NATURAL NUMBERS FROM 1 TO 100 AS 10 NUMBERS PER LINE BY USING FOR STATEMENTS.
+
+/*import java.util.Scanner;
+public class Displaynumbersperline{
+    public static void main (String[] args) {
+        for(int i=0;i<100;i+=10){
+            for(int j=1;j<=10;j++){
+                System.out.printf("%3d",(i+j));
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+
+//  B-) DISPLAY ODD NATURAL NUMBERS FROM 1 TO 99 AS 5 NUMBERS PER LINE BY USING FOR STATEMENT.
+
+/*import java.util.Scanner;
+public class Displaynumbersperline{
+    public static void main (String[] args) {
+        for(int i=0;i<100;i+=10){
+            for(int j=1;j<10;j+=2){
+                System.out.printf("%3d",(i+j));
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+
+//DıSPLAY EVEN NATURAL NUMBERS FROM 2 TO 100 AS 5 NUMBERS PER LINE BY USING FOR STATEMENT.
+
+/*import java.util.Scanner;
+public class Displaynumbersperline{
+    public static void main (String[] args) {
+        for(int i=0;i<100;i+=10){
+            for(int j=2;j<=10;j+=2){
+                System.out.printf("%3d",(i+j));
+            }
+            System.out.println();
+        }
+    }
+}*/
