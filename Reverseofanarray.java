866822058f83669f75d5ae0792b8cc89d1093cb3
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-30 23:54:28 +0300

    added Reverseofanarray.java

---
+++ Reverseofanarray.java
@@ -0,0 +1,26 @@
+//  CODE THE PROGRAM THAT CREATES AND DISPLAYS THE REVERSE OF AN ARRAY WITH 15 ELEMENTS AS ANOTHER ARRAY.
+
+/*import java.util.Scanner;
+public class Reverseofanarray{
+    public static void main (String[] args) {
+        Scanner get=new Scanner(System.in);
+        int array[]=new int[15];
+        for(int i=0;i<15;i++){
+            System.out.println("enter a number:");
+            array[i]=get.nextInt();
+        }
+        int reversedarray[]=new int[15];
+        for(int i=0;i<15;i++){
+            reversedarray[i]=array[15-i-1];
+        }
+        System.out.println("ARRAY:");
+        for(int i=0;i<15;i++){
+            System.out.printf("%2d",array[i]);
+        }
+        System.out.println();
+        System.out.println("REVERSED ARRAY:");
+        for(int i=0;i<15;i++){
+            System.out.printf("%2d",reversedarray[i]);
+        }
+    }
+}*/
