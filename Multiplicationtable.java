fb993554c84c068672c5e72ba3c830c57d05b60e
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-04-30 23:54:11 +0300

    added Multiplicationtable.java

---
+++ Multiplicationtable.java
@@ -0,0 +1,14 @@
+//DISPLAY THE MULTIPLICATION TABLE UNTIL 10 (AS 10X10 TABLE).
+
+/*import java.util.Scanner;
+public class Multiplicationtable{
+    public static void main (String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.printf("     %d     ",i);
+            for(int j=1;j<=10;j++){
+                System.out.printf("%2d x%2d=%3d    ",i,j,(i*j));
+            }
+            System.out.println();
+        }
+    }
+}*/
