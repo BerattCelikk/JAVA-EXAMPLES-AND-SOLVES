213adccdca88381d85d1c21e12462501fed58707
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-04 20:19:59 +0300

    added Onlynumbers.java

---
+++ Onlynumbers.java
@@ -0,0 +1,17 @@
+//WRITE A METHOD THAT WRITES THE NUMBERS CONTAINING STRING CHARACTERS AS NUMBERS ONLY.
+//EX: 6a1b9c -> 619
+
+/*import java.util.Scanner;
+public class Onlynumbers{
+    public static void main (String[] args) {
+        Scanner get=new Scanner(System.in);
+        System.out.println("enter a string:");
+        String string = get.nextLine();
+        for(int i=0;i<string.length();i++){
+            char c=string.charAt(i);
+            if((c>='0')&&(c<='9')){
+                System.out.print(c);
+            }
+        }
+    }
+}*/
