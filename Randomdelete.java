c0c55631f02a3273b7ad260d2e8faa5e4c357667
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-04 20:20:45 +0300

    added Randomdelete.java

---
+++ Randomdelete.java
@@ -0,0 +1,19 @@
+//WRITE A METHOD THAT WILL DELETE ANY CHARACTER IN A STRING.
+
+/*import java.util.Scanner;
+
+public class Randomdelete{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+
+        System.out.println("Enter the character to delete:");
+        char charToDelete = scanner.next().charAt(0);
+
+        String result = inputString.replaceAll(String.valueOf(charToDelete), "");
+
+        System.out.println("Result after deleting '" + charToDelete + "': " + result);
+    }
+}
+*/
