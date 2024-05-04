e874ff60be41a2fdbd8caf843aadffcf15ef7458
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-04 20:21:06 +0300

    added Sumprimenumbers.java

---
+++ Sumprimenumbers.java
@@ -0,0 +1,27 @@
+//WRITE A METHOD THAT PRINTS THE SUM OF PRIME NUMBERS UP TO 1000.
+
+/*import java.util.Scanner;
+public class Sumprimenumbers{
+    public static boolean isPrime(int number){
+        if(number<=1){
+            return false;
+        }
+        for(int i=2;i<number;i++){
+            if(number%i==0)
+                return false;
+        }
+        if(number==2){
+            return true;
+        }
+        return true;
+    }
+    public static void main (String[] args) {
+        int sum=0;
+        for(int i=2;i<=1000;i++){
+            if(isPrime(i)){
+                sum+=i;
+            }
+        }
+        System.out.printf("sum of prime numbers up to 1000 = %d",sum);
+    }
+}*/
