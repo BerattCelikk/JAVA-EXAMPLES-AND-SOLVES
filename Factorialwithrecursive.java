3483aaff8c07589467a3007b8c1a026ad3d587da
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-05-04 20:19:17 +0300

    added Factorialwithrecursive.java

---
+++ Factorialwithrecursive.java
@@ -0,0 +1,18 @@
+//PROGRAM THAT FIND FACTORIAL OF N WITH RECURSIVE FUNCTION (USER WILL ENTER N).
+
+/*import java.util.Scanner;
+public class Factorialwithrecursive{
+    public static int factorial(int number){
+        int factorial=1;
+        for(int i=1;i<=number;i++){
+            factorial*=i;
+        }
+        return factorial;
+    }
+    public static void main (String[] args) {
+        Scanner get=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int number=get.nextInt();
+        System.out.println("factorial of "+number+" = "+factorial(number));
+    }
+}*/
